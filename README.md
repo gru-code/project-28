91c99ab8563d4534f3a6a5db1b50dd00cfaeac4d
gru-code <83279363+gru-code@users.noreply.github.com>
2021-08-16 16:13:54 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# project-28
+# Geologist
+Geologist Project
---
+++ Stone.js
@@ -0,0 +1,13 @@
+class Stone{
+        constructor(x,y,r)
+        {
+                var options={
+            restitution:0.8,
+            friction:0.9,
+            density:12
+        }
+    }
+        display(){
+            
+        }
+}
